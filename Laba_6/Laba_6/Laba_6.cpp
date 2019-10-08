10853e1a6b8c772cf5f6c66f49b387d2cb9d78a0
Nazyrick <ulasevich_nazar@mail.ru>
2019-10-08 12:15:26 +0300

    Laba 6 complete

--- Laba_6/Laba_6/Laba_6.cpp
+++ Laba_6/Laba_6/Laba_6.cpp
@@ -1,8 +1,23 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float f = 125e-6, y = 1.7, t = 1, c, d;
+	while (t < 2.1)
+	{
+		c = 2 * sin(f / 2) + log10(t);
+		t = t + 0.1;
+	}
+	if (c >= 3) {
+		d= exp(-2 * t) + f;
+		cout << d;
+	}else if (c < 3){
+		d = y - y * y * y;
+		cout << d;
+	}
+		
+
 }
 
 
