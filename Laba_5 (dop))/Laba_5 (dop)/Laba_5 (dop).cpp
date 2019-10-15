6a7c637d6fb8687d61ea9f64b184ad1077836ecd
Nazyrick <ulasevich_nazar@mail.ru>
2019-10-15 18:28:40 +0300

    Laba_7 complete

---
+++ Laba_5 (dop))/Laba_5 (dop)/Laba_5 (dop).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	float p=0, q=0, x = 1;
+	cout << "Введите p" << p << endl; cin >> p;
+	cout << "Введите q" << q << endl; cin >> q;
+	for (p; p <= q; p += 0.03 * p) {
+		if (p > q) {
+			
+		}
+		x++;
+	}
+	cout << "Выручка фирмы" << p << endl;
+	cout << "Количество дней" << x;
+
+
+	 
+	}
+
+
+
+
+
+
+
+s
+
--- Laba_7/Laba_7/Laba_7.cpp
+++ Laba_7/Laba_7/Laba_7.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	setlocale(LC_CTYPE, "Russian" );
-	float x,sum = 0, max = 0, y = 0 ;
+	float x,  max = 0, y = 0 ;
 	const int n = 5;
 	for (int i = 1; i <= n ; i++)
 	{
@@ -18,6 +18,21 @@ int main()
 	}
 
 	cout << "" << y * max << endl;
-
+	 
+	/*float z, a = 0, b = 1;
+	const int n = 5;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "Введите значение z "  << endl;
+		cin >> z;
+		if (z > 0)
+			a = a + z;
+		else if (z <= 0)
+		{
+			b = b * z;
+		}
+		cout << "Значение суммы:  " << a << endl;
+		cout << "Значение произведения:  " << b << endl;
+	}*/
 }
 
